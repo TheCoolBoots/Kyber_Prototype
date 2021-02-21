61e677bfaad5d3ef82ac6a65c07afaa718d2b3bb
Lawrence Goo <lawrencegoo53@gmail.com>
2021-02-21 12:21:29 -0800

    Rigged RC Car and added a car controller

---
+++ Kyber_Furniture/Assets/Scripts/RC_Car_scripts/CarCam.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarCam : MonoBehaviour
+{
+    public Camera useCamera;
+    public Transform trackObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        useCamera.transform.LookAt(trackObject);
+    }
+}
