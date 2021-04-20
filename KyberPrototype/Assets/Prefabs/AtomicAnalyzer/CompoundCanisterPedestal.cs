44a32e33af66e5204ce5f0708ead33560b61a72f
Andrew Lai <arjlai221@gmail.com>
2021-04-20 16:54:38 -0700

    added pseudocode for snap point

---
+++ KyberPrototype/Assets/Prefabs/AtomicAnalyzer/CompoundCanisterPedestal.cs
@@ -0,0 +1,18 @@
+
+
+/*
+    
+
+
+    if object colliding is compoundCanister
+        if hand is attatched && shadow is uninstantiated
+            create shadow
+        else if hand is not attached
+            if shadow exists
+                kill shadow
+            snap canister to snap point
+        
+            
+            
+
+*/
