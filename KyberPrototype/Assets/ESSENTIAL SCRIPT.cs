514b4bc1a98a7aa1590d1ab26af36bce3f248117
Andrew Lai <arjlai221@gmail.com>
2021-05-09 16:10:32 -0700

    David Man is kinda hot

---
+++ KyberPrototype/Assets/ESSENTIAL SCRIPT.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ESSENTIALSCRIPT : MonoBehaviour
+{
+    // THIS IS A CRITICAL SCRIPT NECCESSARY FOR PROJECT TO FUNCTION
+
+}
