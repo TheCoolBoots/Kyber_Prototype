4480a42be31125a9922b99eb037fea61a513df27
Andrew Lai <arjlai221@gmail.com>
2021-05-13 09:33:16 -0700

    Canister redesign incomplete, merging changes from main

---
+++ KyberPrototype/Assets/Editor/AddInvertedMeshColliderEditor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(AddInvertedMeshCollider))]
+public class AddInvertedMeshColliderEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        AddInvertedMeshCollider script = (AddInvertedMeshCollider)target;
+        if (GUILayout.Button("Create Inverted Mesh Collider"))
+            script.CreateInvertedMeshCollider();
+    }
+}
---
+++ KyberPrototype/Assets/Scripts/AddInvertedMeshCollider.cs
@@ -0,0 +1,33 @@
+﻿
+
+using UnityEngine;
+using System.Linq;
+using System.Collections;
+
+public class AddInvertedMeshCollider : MonoBehaviour
+{
+    public bool removeExistingColliders = true;
+
+    public void CreateInvertedMeshCollider()
+    {
+        if (removeExistingColliders)
+            RemoveExistingColliders();
+
+        InvertMesh();
+
+        gameObject.AddComponent<MeshCollider>();
+    }
+
+    private void RemoveExistingColliders()
+    {
+        Collider[] colliders = GetComponents<Collider>();
+        for (int i = 0; i < colliders.Length; i++)
+            DestroyImmediate(colliders[i]);
+    }
+
+    private void InvertMesh()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().sharedMesh;
+        mesh.triangles = mesh.triangles.Reverse().ToArray();
+    }
+}
