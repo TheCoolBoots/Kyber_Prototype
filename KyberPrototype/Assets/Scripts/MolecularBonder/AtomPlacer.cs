66760d9169edc3bca8af2d91c86925736136382e
David Man <davman2525@gmail.com>
2021-06-10 18:44:53 -0400

    generate atom and creating placement scripts

---
+++ KyberPrototype/Assets/Scripts/MolecularBonder/AtomPlacer.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class AtomPlacer : MonoBehaviour
+{
+    public int gridPoint;
+    public GameObject atom;
+
+    void Start()
+    {
+        placeAtom();
+    }
+
+    private void Update()
+    {
+
+    }
+
+    public void placeAtom()
+    {
+        if (gridPoint == 1)
+        {
+            Instantiate(atom, new Vector3(-1, 0, 3), Quaternion.identity);
+        }
+        if (gridPoint == 2)
+        {
+            Instantiate(atom, new Vector3(-1, 1, 3), Quaternion.identity);
+        }
+        if (gridPoint == 3)
+        {
+            Instantiate(atom, new Vector3(-1, 2, 3), Quaternion.identity);
+        }
+        if (gridPoint == 4)
+        {
+            Instantiate(atom, new Vector3(0, 0, 3), Quaternion.identity);
+        }
+        if (gridPoint == 5)
+        {
+            Instantiate(atom, new Vector3(0, 1, 3), Quaternion.identity);
+        }
+        if (gridPoint == 6)
+        {
+            Instantiate(atom, new Vector3(0, 2, 3), Quaternion.identity);
+        }
+        if (gridPoint == 7)
+        {
+            Instantiate(atom, new Vector3(1, 0, 3), Quaternion.identity);
+        }
+        if (gridPoint == 8)
+        {
+            Instantiate(atom, new Vector3(1, 1, 3), Quaternion.identity);
+        }
+        if (gridPoint == 9)
+        {
+            Instantiate(atom, new Vector3(1, 2, 3), Quaternion.identity);
+        }
+    }
+}
---
+++ KyberPrototype/Assets/Scripts/MolecularBonder/GenerateAtom.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateAtom : MonoBehaviour
+{
+    public static GenerateAtom Instance;
+    public GameObject atom;
+
+    void Start()
+    {
+
+    }
+
+    public void generateAtom()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+            Instantiate(atom, new Vector3(-1, 1, 0), Quaternion.identity);
+        }
+        else if (this != Instance)
+        {
+            Debug.Log("Destroying extra GM");
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class GenerateAtom : MonoBehaviour
+{
+    public GameObject myPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void generateAtom()
+    {
+        if (GameObject.Find("myPrefab"))
+        {
+
+        }
+        else
+        {
+            Instantiate(myPrefab, new Vector3(-1, 1, 0), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+*/
