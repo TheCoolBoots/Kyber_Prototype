e83c06ffff0107251dc4940d01cae34c0c845b2d
Lawrence Goo <lawrencegoo53@gmail.com>
2021-06-09 20:49:49 -0700

    Updated water fog #17 , caustics #18 , and lightshafts #36

---
+++ KyberPrototype/Assets/Scripts/ImageEffects/UpdateLight.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+[ExecuteInEditMode]
+public class UpdateLight : MonoBehaviour
+{
+    public Transform mainLight;
+
+    [SerializeField]
+    private VisualEffect lightRay;
+    private Vector3 sourceAngle;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        sourceAngle = mainLight.eulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sourceAngle = mainLight.eulerAngles;
+        if (lightRay.GetVector3("LightAngle") != sourceAngle)
+        {
+            lightRay.SetVector3("LightAngle", sourceAngle);
+        }
+    }
+}
