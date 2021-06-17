09e76fa44b55fdb5b26a0980ef68b39b21d52353
David Man <davman2525@gmail.com>
2021-06-17 15:29:44 -0400

    created microscope prefab and added to minisub prefab folder

---
+++ KyberPrototype/Assets/Scripts/Misc/ImageAppear.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageAppear : MonoBehaviour
+{
+    public Image image;
+    void Start()
+    {
+        image.enabled = false;
+    }
+
+    public void makeImageAppear()
+    {
+        image.enabled = true;
+    }
+
+    public void makeImageDisappear()
+    {
+        image.enabled = false;
+    }
+}
