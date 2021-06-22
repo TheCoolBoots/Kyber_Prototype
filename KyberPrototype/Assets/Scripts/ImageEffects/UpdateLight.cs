9f1c58510174088a1993c54975aa5fee4731cb0b
Lawrence Goo <lawrencegoo53@gmail.com>
2021-06-22 14:07:47 -0700

    Added some water effects

--- KyberPrototype/Assets/Scripts/ImageEffects/UpdateLight.cs
+++ KyberPrototype/Assets/Scripts/ImageEffects/UpdateLight.cs
@@ -2,28 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.VFX;
+using UnityEditor;
 
 [ExecuteInEditMode]
 public class UpdateLight : MonoBehaviour
 {
-    public Transform mainLight;
+    public Light mainLight;
+    public Light oceanLight;
+    public Material skybox;
 
     [SerializeField]
     private VisualEffect lightRay;
+    private Transform mainLightPos;
     private Vector3 sourceAngle;
+    private Vector3 cameraPos;
+    private Vector3 lastCameraPos;
+    private float depthBound = 50f;
     // Start is called before the first frame update
     void Awake()
     {
-        sourceAngle = mainLight.eulerAngles;
+        mainLightPos = mainLight.transform;
+        sourceAngle = mainLightPos.eulerAngles;
     }
 
     // Update is called once per frame
     void Update()
     {
-        sourceAngle = mainLight.eulerAngles;
+        sourceAngle = mainLightPos.eulerAngles;
         if (lightRay.GetVector3("LightAngle") != sourceAngle)
         {
             lightRay.SetVector3("LightAngle", sourceAngle);
         }
+
+        if (Camera.current != null)
+        {
+            cameraPos = Camera.current.transform.position;
+            //cameraPos = SceneView.currentDrawingSceneView.camera.transform.position;
+            //print(cameraPos);
+        }
+        else if (Application.isFocused)
+        {
+            cameraPos = Camera.main.transform.position;
+        }
+        
+        if (cameraPos != lastCameraPos)
+        {
+            float mainLightIntensity = Mathf.Clamp01((depthBound + cameraPos.y) / depthBound)*1.4f;
+            mainLight.intensity = mainLightIntensity;
+            float oceanLightIntensity = Mathf.Clamp01((-cameraPos.y) / depthBound);
+            oceanLight.intensity = oceanLightIntensity;
+            lastCameraPos = cameraPos;
+            
+            //skybox.shader.
+            //mainLight.intensity = cameraPos.y
+        }
+        //print(Application.isFocused);
     }
 }
