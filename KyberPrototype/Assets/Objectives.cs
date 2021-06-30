b9dca5ba8cdaa902d3f5a0310bfaae90aaf88ac6
Derek D <dereksduenas@gmail.com>
2021-06-30 15:20:10 -0700

    Added Objective List prototype

---
+++ KyberPrototype/Assets/Objectives.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objectives : MonoBehaviour
+{
+    private CharacterController controller;
+
+    public void ToggleVisibilty()
+    {
+        Renderer rend = gameObject.GetComponent<Renderer>();
+
+        if(rend.enabled)
+        {
+            rend.enabled = false;
+        }
+        else
+        {
+            rend.enabled = true;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("XRI_Left_PrimaryTouch"))
+            ToggleVisibilty();
+
+    }
+}
